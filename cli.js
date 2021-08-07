b51fede80735f4db76d1cc3d03f4215e783b75a3
Christian Ruink <christianruink@christians-mbp.lan>
2021-08-06 23:34:01 -0700

    Adding tests and boilerplate for various casses

--- cli.js
+++ cli.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 // include node fs module
-const fs = require('fs');
-const prompt = require('prompt');
+import fs, { promises } from 'fs';
+import prompt from 'prompt';
 
 const { writeFile, mkdir } = fs;
 
-const properties = [
+const coreProperties = [
     {
-        name: 'name',
+        name: 'componentName',
         validator: /^[a-zA-Z\s]+$/,
         message: 'Enter new Component name',
         error: 'Component name must only be letters'
@@ -16,44 +16,79 @@ const properties = [
     {
         name: 'filepath',
         message: 'Enter filepath to provision (default \'./\')'
+    },
+];
+
+const extraProperties = [
+    {
+        name: 'native',
+        message: 'Is it a React Native component? (t/f)',
+        type: 'boolean',
+    },
+    {
+        name: 'typescript',
+        message: 'Using typescript? (t/f)',
+        type: 'boolean',
+    },
+    {
+        name: 'storybook',
+        message: 'Add storybook? (t/f)',
+        type: 'boolean',
+    },
+    {
+        name: 'styledComponents',
+        message: 'Add styled components? (t/f)',
+        type: 'boolean',
+    },
+    {
+        name: 'tests',
+        message: 'Add tests? (t/f)',
+        type: 'boolean'
     }
 ];
 
- 
-const makeComponentBoilerPlayer = async () => {
-    prompt.get(properties, async (err, result) => {
-        if (err) { return onErr(err); }
+export const createComponentDirectory = async ({ componentName, filepath }) => {
+    const directoryName = `${filepath}${componentName}`;
+    await promises.mkdir(directoryName);
+};
 
-        console.log('result: ', result);
-        
-        const { name = 'Component', filepath = './' } = result;
+export const createComponentFile = async ({ componentName, filepath, native, typescript, styledComponents }) => {
+    const componentFileName = `${filepath}${componentName}/${componentName}.${typescript ? 'tsx' : 'jsx'}`;
 
-        const directoryName = `${filepath}${name}`
-        await mkdir(directoryName, (data) => console.log('data: ', data));
-        const componentFileName = `${filepath}${name}/${name}.tsx`;
-
-        const componentBoilerplate = `
-import React from 'react';
-import StyledComponents from './${name}Styled';
+    const styledComponentsImportChunk = `import StyledComponents from './${componentName}Styled';`;
+    const nativeImportChunk = `import { View } from 'react-native';`
+    
+    const styledComponentsDestructuringChunk = `
 const {
     Container,
 } = StyledComponents;
-const ${name} = () => {
+`;
+
+    const componentBoilerplate = `
+import React from 'react';
+${styledComponents ? styledComponentsImportChunk : native ? nativeImportChunk : ''}
+${styledComponents ? styledComponentsDestructuringChunk : ''}
+
+const ${componentName} = () => {
     return (
-        <Container />
+        <${styledComponents ? 'Container' : native ? 'View' : 'div'} />
     );
 };
 
-export default ${name};
+export default ${componentName};
 `;
-        
-        await writeFile(componentFileName, componentBoilerplate, (data) => console.log('data: ', data));
 
-        const stylesFileName = `${filepath}${name}/${name}Styled.tsx`;
+    await promises.writeFile(componentFileName, componentBoilerplate);
+};
 
-        const stylesBoilerplate = `
-import styled from 'styled-components/native';
-const Container = styled.View\`\`;
+export const createStylesFile = async ({ componentName, filepath, typescript, native }) => {
+    const stylesFileName = `${filepath}${componentName}/${componentName}Styled.${typescript ? 'tsx' : 'jsx'}`;
+
+    const stylesBoilerplate = `
+import styled from 'styled-components${native ? '/native' : ''}';
+
+const Container = styled.${native ? 'View' : 'div'}\`\`;
+
 const StyledComponents = {
     Container,
 };
@@ -61,30 +96,136 @@ const StyledComponents = {
 export default StyledComponents;
 `;
 
-        await writeFile(stylesFileName, stylesBoilerplate, (data) => console.log('data: ', data));
-
-        const storybookFileName = `${filepath}${name}/${name}.stories.tsx`;
-
-        const storiesBoilerplate = `
-import React from 'react';
-import ${name} from './${name}';
-import { storiesOf } from '@storybook/react-native';
-storiesOf('${name}', module)
-.add('default', () => (
-    <${name} />
-));
-`;
-        
-    await writeFile(storybookFileName, storiesBoilerplate, (data) => console.log('data: ', data));
-    });
-
+    await promises.writeFile(stylesFileName, stylesBoilerplate);
 };
 
-prompt.start()
+export const createStorybookFile = async ({ componentName, filepath, native, typescript }) => {
+    const storybookFileName = `${filepath}${componentName}/${componentName}.stories.${typescript ? 'tsx' : 'jsx'}`;
+
+    const storiesNativeBoilerplate = `
+import React from 'react';
+import ${componentName} from './${componentName}';
+import { storiesOf } from '@storybook/react-native';
+
+storiesOf('${componentName}', module).add('default', () => (
+    <${componentName} />
+));
+`;
+
+    const storiesWebBoilerplate = `
+import React from 'react';
+import ${componentName} from './${componentName}';
+
+export default {
+  title: '${componentName}',
+  component: ${componentName},
+  argTypes: {},
+};
+
+const Template = (args) => <${componentName} {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+};
+`;
+
+    await promises.writeFile(storybookFileName, native ? storiesNativeBoilerplate : storiesWebBoilerplate);
+}
+
+export const createTestsFile = async ({ componentName, filepath, typescript }) => {
+    const testsDirectoryName = `${filepath}${componentName}/__tests__/`;
+    await promises.mkdir(testsDirectoryName);
+    
+    const testFileName = `${filepath}${componentName}/__tests__/${componentName}.test.${typescript ? 'tsx' : 'jsx'}`;
+
+    const testBoilerplate = `
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ${componentName} from '../${componentName}';
+
+test('renders correctly', () => {
+    const tree = renderer.create(<${componentName} />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+`;
+    await promises.writeFile(testFileName, testBoilerplate); 
+};
+ 
+export const main = async () => {
+    prompt.start();
+    let config = await import('./insta-component.config.js');
+    
+    prompt.get(coreProperties, async (err, result) => {
+        if (err) { return onErr(err); }
+        const { name, filepath } = coreProperties;
+
+        // if not config object prompt needed
+        if(config) {
+            prompt.get(extraProperties, async (err, result) => {
+                if (err) { return onErr(err); }
+                
+                await writeConfigFile(extraProperties);
+                const args = { name, filepath, ...extraProperties}
+                await writeBoilerplate(args);
+            });
+        } else {
+            const args = { name, filepath, ...config };
+            await writeBoilerplate(args);
+        }
+    });
+};
+
+export const writeBoilerplate = async (args) => {
+    const { 
+        componentName = 'Component', 
+        filepath = './',
+        native = false,
+        typescript = false,
+        storybook = false,
+        styledComponents = false,
+        tests = false,
+    } = args;
+
+    
+    await createComponentDirectory({ componentName, filepath });
+    await createComponentFile({ componentName, filepath, native, typescript, styledComponents});
+    
+    if(styledComponents) await createStylesFile({ componentName, filepath, typescript, native});
+    if(storybook) await createStorybookFile({ componentName, filepath, typescript, native });
+    if(tests) await createTestsFile({ componentName, filepath, typescript});
+};
+
+
+// When there is no config file and CLI is used for the first time to create boilerplate
+export const writeConfigFile = async ({ native, typescript, styledComponents, storybook, tests }) => {
+    
+
+    const configFileBoilerplate = `
+const config = {
+    native: ${native[0].toLowerCase() === 't'},
+    typescript: ${typescript[0].toLowerCase() === 't'},
+    storybook: ${storybook[0].toLowerCase() === 't'},
+    styledComponents: ${styledComponents[0].toLowerCase() === 't'},
+    tests: ${tests[0].toLowerCase() === 't'},
+};
+
+export default config;
+`
+    await promises.writeFile('./insta-component.config.js', configFileBoilerplate);
+}
 
 const onErr = (err) => {
     console.log(err);
     return 1;
-}
+};
 
-makeComponentBoilerPlayer();
+export default {
+    createComponentDirectory,
+    createComponentFile,
+    createStorybookFile,
+    createStylesFile,
+    createTestsFile,
+    writeBoilerplate,
+    writeConfigFile,
+};
---
+++ test/cli-test.js
@@ -0,0 +1,309 @@
+import assert from 'assert';
+import fs, { existsSync, promises, write } from 'fs';
+import InstaComponent from '../cli.js';
+
+const {
+    createComponentDirectory,
+    createStylesFile,
+    createComponentFile,
+    createStorybookFile,
+    createTestsFile,
+    writeBoilerplate,
+} = InstaComponent;
+
+describe('Create Component Directory', () => {
+    afterEach(async () => {
+        await fs.rmdir('./test/tmp-test', () => null);
+    })
+
+    it('should create a directory with the name provided in filepath provided', async () => {
+        await createComponentDirectory({ componentName: 'tmp-test', filepath: './test/' });
+        assert.ok(fs.existsSync('./test/tmp-test'));
+    });
+}); 
+
+describe('Create Component File', () => {
+    before(async () => {
+        await promises.mkdir('./test/TestComponent');
+    });
+
+    after(async () => {
+        await promises.rmdir('./test/TestComponent/');
+    });
+
+
+    it('should create a react web component with styled-components and tsx filetype', async () => {
+        await createComponentFile({ 
+            componentName: 'TestComponent', 
+            filepath: './test/', 
+            typescript: true, 
+            native: false, 
+            styledComponents: true
+        });
+        // file exists
+        const exists = await fs.existsSync('./test/TestComponent/TestComponent.tsx');
+        assert.ok(exists);
+
+        const file = await promises.readFile('./test/TestComponent/TestComponent.tsx', 'utf-8');
+        assert.ok(file.includes("import StyledComponents from './TestComponentStyled';"));
+        assert.ok(file.includes("<Container />"));
+
+        await promises.rm('./test/TestComponent/TestComponent.tsx');
+    });
+
+    it('should create a react web component with no styled-components and tsx filetype', async () => {
+        await createComponentFile({ 
+            componentName: 'TestComponent', 
+            filepath: './test/', 
+            typescript: true, 
+            native: false, 
+            styledComponents: false
+        });
+        // file exists
+        const exists = await fs.existsSync('./test/TestComponent/TestComponent.tsx');
+        assert.ok(exists);
+
+        const file = await promises.readFile('./test/TestComponent/TestComponent.tsx', 'utf-8');
+        assert.ok(file.includes("<div />"));
+
+        await promises.rm('./test/TestComponent/TestComponent.tsx');
+    });
+
+    it('should create a react-native component with styled-components', async () => {
+        await createComponentFile({ 
+            componentName: 'TestComponent', 
+            filepath: './test/', 
+            typescript: true, 
+            native: true, 
+            styledComponents: true
+        });
+        // file exists
+        const exists = await fs.existsSync('./test/TestComponent/TestComponent.tsx');
+        assert.ok(exists);
+
+        const file = await promises.readFile('./test/TestComponent/TestComponent.tsx', 'utf-8');
+        assert.ok(file.includes("import StyledComponents from './TestComponentStyled';"));
+        assert.ok(file.includes("<Container />"));
+
+        await promises.rm('./test/TestComponent/TestComponent.tsx');
+    });
+
+    it('should create a react-native component with no styled-components', async () => {
+        await createComponentFile({ 
+            componentName: 'TestComponent', 
+            filepath: './test/', 
+            typescript: true, 
+            native: true, 
+            styledComponents: false
+        });
+        // file exists
+        const exists = await fs.existsSync('./test/TestComponent/TestComponent.tsx');
+        assert.ok(exists);
+
+        const file = await promises.readFile('./test/TestComponent/TestComponent.tsx', 'utf-8');
+        assert.ok(file.includes("import { View } from 'react-native';"));
+        assert.ok(file.includes("<View />"));
+
+        await promises.rm('./test/TestComponent/TestComponent.tsx');
+    });
+}); 
+
+describe('Create Styles File', () => {
+    before(async () => {
+        await promises.mkdir('./test/TestComponent');
+    });
+
+    after(async () => {
+        await promises.rmdir('./test/TestComponent/');
+    });
+
+
+    it('should create a react web styles file with tsx filetype', async () => {
+        const result = await createStylesFile({ componentName: 'TestComponent', filepath: './test/', typescript: true, native: false });
+        // file exists
+        const exists = await fs.existsSync('./test/TestComponent/TestComponentStyled.tsx');
+        assert.ok(exists);
+        const file = await promises.readFile('./test/TestComponent/TestComponentStyled.tsx', 'utf-8', () => null);
+        assert.ok(file.includes("import styled from 'styled-components';"));
+        assert.ok(file.includes("styled.div``"));
+
+        await promises.rm('./test/TestComponent/TestComponentStyled.tsx');
+    });
+
+    it('should create a react web styles file with jsx filetype', async () => {
+        const result = await createStylesFile({ componentName: 'TestComponent', filepath: './test/', typescript: false, native: false });
+        // file exists
+        const exists = await fs.existsSync('./test/TestComponent/TestComponentStyled.jsx');
+        assert.ok(exists);
+        const file = await promises.readFile('./test/TestComponent/TestComponentStyled.jsx', 'utf-8', () => null);
+
+        assert.ok(file.includes("import styled from 'styled-components';"));
+        assert.ok(file.includes("styled.div``"));
+
+        await promises.rm('./test/TestComponent/TestComponentStyled.jsx');
+    });
+
+    it('should create a react native styles file with tsx filetype', async () => {
+        const result = await createStylesFile({ componentName: 'TestComponent', filepath: './test/', typescript: true, native: true });
+        // file exists
+        const exists = await fs.existsSync('./test/TestComponent/TestComponentStyled.tsx');
+
+        assert.ok(exists);
+        const file = await promises.readFile('./test/TestComponent/TestComponentStyled.tsx', 'utf-8', () => null);
+
+        assert.ok(file.includes("import styled from 'styled-components/native';"));
+        assert.ok(file.includes("styled.View``"));
+
+        await promises.rm('./test/TestComponent/TestComponentStyled.tsx');
+    });
+
+    it('should create a react-native styles file with jsx filetype', async () => {
+        const result = await createStylesFile({ componentName: 'TestComponent', filepath: './test/', typescript: false, native: true });
+        // file exists
+        const exists = await fs.existsSync('./test/TestComponent/TestComponentStyled.jsx');
+
+        assert.ok(exists);
+        const file = await promises.readFile('./test/TestComponent/TestComponentStyled.jsx', 'utf-8', () => null);
+
+        assert.ok(file.includes("import styled from 'styled-components/native';"));
+        assert.ok(file.includes("styled.View``"));
+
+        await promises.rm('./test/TestComponent/TestComponentStyled.jsx');
+    });
+}); 
+
+describe('Create Storybook File', () => {
+    before(async () => {
+        await promises.mkdir('./test/TestComponent');
+    });
+
+    after(async () => {
+        await promises.rmdir('./test/TestComponent/');
+    });
+
+
+    it('should create a jsx file', async () => {
+        await createStorybookFile({ 
+            componentName: 'TestComponent', 
+            filepath: './test/', 
+            typescript: false, 
+            native: false, 
+        });
+        // file exists
+        const exists = await fs.existsSync('./test/TestComponent/TestComponent.stories.jsx');
+        assert.ok(exists);
+        await promises.rm('./test/TestComponent/TestComponent.stories.jsx');
+    });
+
+    it('should create a storybook file for react-web', async () => {
+        await createStorybookFile({ 
+            componentName: 'TestComponent', 
+            filepath: './test/', 
+            typescript: true, 
+            native: false, 
+        });
+
+        // file exists
+        const exists = await fs.existsSync('./test/TestComponent/TestComponent.stories.tsx');
+        assert.ok(exists);
+
+        const file = await promises.readFile('./test/TestComponent/TestComponent.stories.tsx', 'utf-8');
+        assert.ok(file.includes("const Template = (args) => <TestComponent {...args} />;"));
+
+        await promises.rm('./test/TestComponent/TestComponent.stories.tsx');
+    });
+
+    it('should create a storybook file for react-native', async () => {
+        await createStorybookFile({ 
+            componentName: 'TestComponent', 
+            filepath: './test/', 
+            typescript: true, 
+            native: true, 
+        });
+        // file exists
+        const exists = await fs.existsSync('./test/TestComponent/TestComponent.stories.tsx');
+        assert.ok(exists);
+
+        const file = await promises.readFile('./test/TestComponent/TestComponent.stories.tsx', 'utf-8');
+        assert.ok(file.includes("import { storiesOf } from '@storybook/react-native';"));
+
+        await promises.rm('./test/TestComponent/TestComponent.stories.tsx');
+    });
+}); 
+
+describe('Create a test File', () => {
+    before(async () => {
+        await promises.mkdir('./test/TestComponent');
+    });
+
+    after(async () => {
+        await promises.rmdir('./test/TestComponent/');
+    });
+
+
+    it('should create a test file with jsx', async () => {
+        await createTestsFile({ 
+            componentName: 'TestComponent', 
+            filepath: './test/', 
+            typescript: false, 
+        });
+        // file exists
+        const exists = await fs.existsSync('./test/TestComponent/__tests__/TestComponent.test.jsx');
+        assert.ok(exists);
+        await promises.rm('./test/TestComponent/__tests__/TestComponent.test.jsx');
+        await promises.rmdir('./test/TestComponent/__tests__/');
+    });
+
+    it('should create a test file with tsx', async () => {
+        await createTestsFile({ 
+            componentName: 'TestComponent', 
+            filepath: './test/', 
+            typescript: true, 
+        });
+
+        // file exists
+        const exists = await fs.existsSync('./test/TestComponent/__tests__/TestComponent.test.tsx');
+        assert.ok(exists);
+        await promises.rm('./test/TestComponent/__tests__/TestComponent.test.tsx');
+        await promises.rmdir('./test/TestComponent/__tests__/');
+    });
+}); 
+
+describe('Write all the boilerplate', () => {
+    it('should create all the boilerplate', async () => {
+        await writeBoilerplate({ 
+            componentName: 'TestComponent', 
+            filepath: './test/', 
+            typescript: true, 
+            native: true,
+            styledComponents: true,
+            storybook: true,
+            tests: true,
+        });
+        // directory exists
+        const componentDirectoryExists = await fs.existsSync('./test/TestComponent/');
+        console.log('componentDirectoryExists', componentDirectoryExists);
+        assert.ok(componentDirectoryExists);
+        const componentFileExists = await fs.existsSync('./test/TestComponent/TestComponent.tsx');
+        console.log('componentFileExists', componentFileExists);
+        assert.ok(componentFileExists);
+        const stylesExist = await fs.existsSync('./test/TestComponent/TestComponentStyled.tsx');
+        console.log('stylesExist', stylesExist);
+        assert.ok(stylesExist);
+        const storiesExist = await fs.existsSync('./test/TestComponent/TestComponent.stories.tsx');
+        console.log('storiesExist', storiesExist);
+        assert.ok(storiesExist);
+        const testsExist = await fs.existsSync('./test/TestComponent/__tests__/TestComponent.test.tsx');
+        console.log('testsExist', testsExist);
+        assert.ok(testsExist);
+
+
+        //clean up
+        await promises.rm('./test/TestComponent/TestComponent.tsx');
+        await promises.rm('./test/TestComponent/TestComponentStyled.tsx');
+        await promises.rm('./test/TestComponent/TestComponent.stories.tsx');
+        await promises.rm('./test/TestComponent/__tests__/TestComponent.test.tsx');
+        await promises.rmdir('./test/TestComponent/__tests__/');
+        await promises.rmdir('./test/TestComponent/');
+    });
+});
